98f552a6ff4e3454ac8f02bee4ae718ed81addd9
Shubhang Singh <singhshubhang10906@gmail.com>
2026-07-05 00:41:50 +0530

    first_commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => 
+    {
+        return res.status(200).json({"Hello Shubhang": "Welcome to level 5"});
+    
+    });
+
+    app.get("/health",(req,res) =>
+    {
+        return res.status(200).json({"status": "ok"});
+    })
+
+    app.listen(PORT, () =>
+    {
+        console.log(`Server is running on port ${PORT}`);
+    });
